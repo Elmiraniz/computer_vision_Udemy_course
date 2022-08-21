ca1f6d5e16ec40eddfb913aab8d2ef8b39bdcec7
Elmiraniz <96397248+Elmiraniz@users.noreply.github.com>
2022-08-21 16:51:28 -0400

    script for avoiding kernel crash when closing an image in OpenCVin Mac

---
+++ mypuppy.py
@@ -0,0 +1,14 @@
+# since just showing image in OpenCV in notebook ipynb has the issue when closing the image, we should run the code as a script
+import cv2
+
+path = '/Users/elmiraghahramani/Library/Mobile Documents/com~apple~CloudDocs/Online_courses/Python_for_CV_with_OpenCV_DL/Material/Computer-Vision-with-Python/DATA'
+img = cv2.imread(path+'/00-puppy.jpg')
+
+while True:
+    
+    cv2.imshow('Puppy', img) # window name = puppy
+
+    if cv2.waitKey(1) & 0xFF == 27: # if we waited at least 1ms, AND we've pressed the Esc (symbol/number for Esc button)
+        break
+
+cv2.destroyAllWindows()
