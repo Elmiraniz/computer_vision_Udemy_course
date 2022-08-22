427cc6bea2575c361b9dc502d35fdddea135d6fc
Elmiraniz <96397248+Elmiraniz@users.noreply.github.com>
2022-08-22 09:55:31 -0400

    final version with comments

--- mypuppy.py
+++ mypuppy.py
@@ -1,4 +1,4 @@
-# since just showing image in OpenCV in notebook ipynb has the issue when closing the image, we should run the code as a script
+# since just showing image in OpenCV in notebook ipynb has the issue when closing the image, we should run the code as a script. In this code by pressing Esc, image is closed without error.
 import cv2
 
 path = '/Users/elmiraghahramani/Library/Mobile Documents/com~apple~CloudDocs/Online_courses/Python_for_CV_with_OpenCV_DL/Material/Computer-Vision-with-Python/DATA'
